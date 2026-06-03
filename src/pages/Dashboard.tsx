07be0c3bca4220c103a6d7230a6bf3706aca5f01
kogul-murugaiah <koguldev7@gmail.com>
2026-06-04 00:05:33 +0530

    fix: restore Link import used in Savings Goals section

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useTheme } from "../contexts/ThemeContext";
 import { api } from "../lib/api";
 import { useAccountTypes } from "../hooks/useAccountTypes";
